e48a4ac3579ee03563d3b37cf463c030fe487f88
tenokun <ten-210.10@i.softbank.jp>
2022-07-13 02:27:06 +0900

    データの削除

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -2,13 +2,16 @@
 
 namespace App\Http\Controllers;
 
+
 use App\Post;
 use App\Http\Requests\PostRequest;
+use Illuminate\Http\Request;
 
 class PostController extends Controller
 {
     public function index(Post $post)
     {
+
         return view('index')->with(['posts' => $post->getPaginateByLimit()]);
     }
     
@@ -22,10 +25,30 @@ class PostController extends Controller
         return view('create');
     }
     
-    public function store(PostRequest $request, Post $post)
+    public function store(Request $request, Post $post)
     {
         $input = $request['post'];
         $post->fill($input)->save();
         return redirect('/posts/' . $post->id);
+    }
+    
+    public function edit(Post $post)
+    {
+        return view('edit')->with(['post' => $post]);
+    }
+    
+    public function update(PostRequest $request, Post $post)
+    {
+        $input_post = $request['post'];
+        $post->fill($input_post)->save();
+
+        return redirect('/posts/' . $post->id);
+
+    }
+    
+    public function delete(Post $post)
+    {
+        $post->delete();
+        return redirect('/');
     }
 }
